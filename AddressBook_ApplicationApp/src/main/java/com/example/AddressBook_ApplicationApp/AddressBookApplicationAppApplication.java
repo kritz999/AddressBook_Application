cc1243c2f2069db691a6cbc6586899031a304082
kritz999 <kritikabdas007@gmail.com>
2025-03-02 13:39:21 +0530

    [kritika] add: address book application setup

---
+++ AddressBook_ApplicationApp/src/main/java/com/example/AddressBook_ApplicationApp/AddressBookApplicationAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.AddressBook_ApplicationApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookApplicationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressBookApplicationAppApplication.class, args);
+	}
+
+}
---
+++ AddressBook_ApplicationApp/src/test/java/com/example/AddressBook_ApplicationApp/AddressBookApplicationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AddressBook_ApplicationApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookApplicationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
